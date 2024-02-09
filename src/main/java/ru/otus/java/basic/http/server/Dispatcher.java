bcde83a303d83899206c5cd2f5d5c78ed5026214
dfgnkkr <zhechtec@ya.ru>
2024-02-10 00:28:58 +0300

    урок 31: Основы Web. Пишем простой http клиент и сервер. Часть 1
    // Домашнее задание:
        // - Добавить логирование
        // - Добавить обработку запросов в параллельных потоках

--- src/main/java/ru/otus/java/basic/http/server/Dispatcher.java
+++ src/main/java/ru/otus/java/basic/http/server/Dispatcher.java
@@ -1,5 +1,7 @@
 package ru.otus.java.basic.http.server;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import ru.otus.java.basic.http.server.processors.HelloWorldRequestProcessor;
 import ru.otus.java.basic.http.server.processors.OperationAddRequestProcessor;
 import ru.otus.java.basic.http.server.processors.RequestProcessor;
@@ -14,6 +16,8 @@ public class Dispatcher {
     private Map<String, RequestProcessor> router;
     private RequestProcessor unknownRequestProcessor;
 
+    private static final Logger logger = LogManager.getLogger(Dispatcher.class.getName());
+
     public Dispatcher() {
         this.router = new HashMap<>();
         this.router.put("/add", new OperationAddRequestProcessor());         // /GET /add => OperationAddRequestProcessor
@@ -23,6 +27,7 @@ public class Dispatcher {
 
     public void execute(HttpRequest httpRequest, OutputStream output) throws IOException {
         if (!router.containsKey(httpRequest.getUri())) {
+            logger.warn("Получили неизвестный запрос - показываем страницу 404.");
             unknownRequestProcessor.execute(httpRequest, output);
             return;
         }
--- src/main/java/ru/otus/java/basic/http/server/processors/OperationAddRequestProcessor.java
+++ src/main/java/ru/otus/java/basic/http/server/processors/OperationAddRequestProcessor.java
@@ -1,5 +1,7 @@
 package ru.otus.java.basic.http.server.processors;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import ru.otus.java.basic.http.server.HttpRequest;
 
 import java.io.IOException;
@@ -7,12 +9,24 @@ import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 
 public class OperationAddRequestProcessor implements RequestProcessor {
+
+    private static final Logger logger = LogManager.getLogger(OperationAddRequestProcessor.class.getName());
+
     @Override
     public void execute(HttpRequest httpRequest, OutputStream output) throws IOException {
-        int a = Integer.parseInt(httpRequest.getParameter("a"));
-        int b = Integer.parseInt(httpRequest.getParameter("b"));
-        String result = a + " + " + b + " = " + (a + b);
-        String response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html><body><h1>" + result + "</h1></body></html>";
+        int a = 0;
+        int b = 0;
+        String result = "";
+        try {
+            a = Integer.parseInt(httpRequest.getParameter("a"));
+            b = Integer.parseInt(httpRequest.getParameter("b"));
+            result = a + " + " + b + " = " + (a + b);
+        } catch(NumberFormatException e){
+            logger.error("В запросе отсутсвуют или невалидные параметры. Покажем тогда пользователю альтернативно одарённый текст на странице.");
+            result = "Математика сломалась ;( Пришлите красивые две цифры, пжлста!";
+            e.printStackTrace();
+        }
+        String response = "HTTP/1.1 200 OK\r\nContent-Type: text/html;charset=utf-8\r\n\r\n<html><body><h1>" + result + "</h1></body></html>";
         output.write(response.getBytes(StandardCharsets.UTF_8));
     }
 }
